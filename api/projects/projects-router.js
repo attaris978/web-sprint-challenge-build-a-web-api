d4ee5156681f7c4ea931167180e17ab810256a6f
unknown <ryancare@protonmail.com>
2022-05-12 15:32:52 -0500

    Added post, put, and delete api functions, with error checking that I may break out into a middleware if I can find the strength once my tests are passing.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -16,4 +16,32 @@ projectsRouter.get('/:id', verifyProjectId, (req, res) => {
     .catch(() => res.status(404).json({ message: "No project with that ID exists"}))
 })
 
+projectsRouter.post('/', (req, res) => {
+    const {name, description} = req.body;
+    if (!name || !description) {
+        res.status(400).json({message: "New project entries require name and description fields"})
+    } else {
+        insert(req.body)
+        .then(newProject =>  res.status(201).json(newProject))
+        .catch(() => res.status(500).json({message: "Failed to post new project"}))
+    }
+})
+
+projectsRouter.put('/:id', verifyProjectId, (req, res) => {
+    const {name, description, completed} = req.body;
+    if (!name || !description || typeof completed !== 'boolean') {
+        res.status(400).json({message: "Project updates require name, description, and completed fields"})
+    } else {
+        update(req.params.id, req.body)
+        .then(updatedProject =>  res.status(200).json(updatedProject))
+        .catch(() => res.status(500).json({message: "Failed to update project"}))
+    }
+})
+
+projectsRouter.delete('/:id', verifyProjectId, (req, res) => {
+    remove(req.params.id)
+    .then(deletedProject => res.status(200).json(deletedProject))
+    .catch(() => res.status(500).json({message: "Failed to delete target project"}))
+})
+
 module.exports = projectsRouter;

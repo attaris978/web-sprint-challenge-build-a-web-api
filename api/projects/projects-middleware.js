2fcc6a56d60d818dcd4aed6800ce396f63381fa6
unknown <ryancare@protonmail.com>
2022-05-12 15:12:22 -0500

    Added id-checking project middleware, and get and get by id endpoints

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,17 @@
-// add middlewares here related to projects
+const { get } = require("./projects-model");
+
+function verifyProjectId(req, res, next) {
+    const { id } = req.params;
+    get(id).then((project) => {
+      if (!project) {
+        res.status(404).json({ message: "No project with that id exists" });
+      } else {
+        next();
+      }
+    });
+  }
+  
+  module.exports = {
+    verifyProjectId,
+  };
+  
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,19 @@
 // Write your "projects" router here!
+const express = require('express');
+const {get, insert, update, remove, getProjectActions} = require('./projects-model');
+const {verifyProjectId} = require('./projects-middleware');
+const projectsRouter = express.Router();
+
+projectsRouter.get('/', (req, res) => {
+    get()
+    .then(projects => res.status(200).json(projects))
+    .catch(() => res.status(500).json({ message: "Failed to retrieve the list of projects"}))
+})
+
+projectsRouter.get('/:id', verifyProjectId, (req, res) => {
+    get(req.params.id)
+    .then(projects => res.status(200).json(projects))
+    .catch(() => res.status(404).json({ message: "No project with that ID exists"}))
+})
+
+module.exports = projectsRouter;
